144a894b68b6992adce5a0b90c662bbc46daf740
yogeeswaran473 <yogeeswaran473@gmail.com>
2023-08-10 10:58:45 +0530

    Basic design completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
-import '../src/App.css'
-import logo from "../src/image/favicon.png"
-import playstore from "../src/image/google-play-badge.svg"
-import appstore from "../src/image/app-store-badge.svg"
+import "./App.css";
+import Home from "../src/component/Home";
 
-export default function main(){
+
+function App(){ 
 
   return(
-    <div className="main-container">
-      <h1 className="title">Information<br/> with <br/>Entertainment</h1>
-    <p className="disc">Are you looking to have fun and learn at the same time?</p>
     <div>
-      <img src={logo} alt="" className="logo" />
+    <Home />
     </div>
-    <img src={playstore} alt="" className="playstore" />
-    <img src={appstore} alt="" className="appstore" />
-    </div>
-    )
+  ); 
 }
+export default App;
